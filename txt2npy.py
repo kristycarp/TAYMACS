4055818336b96b1cf6c179faea020e287438e008
kristycarp <kcarp@stanford.edu>
2023-12-18 15:50:30 -0800

    added rest of discography and script for quick converting txt to npy

---
+++ txt2npy.py
@@ -0,0 +1,12 @@
+# this script is just for converting the .txt quotebank into a .npy
+# you don't need to run it unless you want to change the quotes or add to the quotes
+
+import numpy as np
+import os
+
+assert os.path.isfile("tsq.txt")
+f = open("tsq.txt","r").read().split("\n")
+if f[-1] == "":
+    f = f[:-1]
+a = np.array(f)
+np.save("tsq.npy",a)
